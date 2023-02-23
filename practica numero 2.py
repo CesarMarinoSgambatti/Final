859dbcaa739ee5c29f9607c9c85f67332b506dad
CesarMarinoSgambatti <126044805+CesarMarinoSgambatti@users.noreply.github.com>
2023-02-23 14:25:15 +0100

    Create practica numero 2.py

---
+++ practica numero 2.py
@@ -0,0 +1,18 @@
+import math
+
+dato1= int(input("ingrese primer número:   "))
+dato2= int(input("ingrese segundo número:   "))
+dato3= input("ingrese operación: sumar, restar, multiplicar, dividir:  ")
+
+
+if dato3=="sumar":
+    respuesta1=dato1+dato2
+    print(respuesta1)
+if dato3=="restar":
+    respuesta2=dato1-dato2
+    print(respuesta2)
+if dato3=="multiplicar":
+    respuesta3=dato1*dato2
+    print(respuesta3)   
+
+
